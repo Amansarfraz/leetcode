1c3cf0e3d3c132ff3ab16a0d73941e582c55a574
Aman Sarfraz <amansarfraz05@gmail.com>
2026-04-02 22:45:36 +0500

    Time: 48 ms (45.84%) | Memory: 13.6 MB (86.23%) - LeetSync

---
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def pacificAtlantic(self, heights):
+        if not heights or not heights[0]:
+            return []
+        
+        m, n = len(heights), len(heights[0])
+        
+        pacific = set()
+        atlantic = set()
+        
+        def dfs(r, c, visited):
+            visited.add((r, c))
+            directions = [(1,0), (-1,0), (0,1), (0,-1)]
+            
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                
+                if (0 <= nr < m and 0 <= nc < n and
+                    (nr, nc) not in visited and
+                    heights[nr][nc] >= heights[r][c]):
+                    
+                    dfs(nr, nc, visited)
+        
+        # Pacific (top + left)
+        for i in range(m):
+            dfs(i, 0, pacific)
+            dfs(i, n-1, atlantic)
+        
+        for j in range(n):
+            dfs(0, j, pacific)
+            dfs(m-1, j, atlantic)
+        
+        # Intersection
+        return list(pacific & atlantic)
