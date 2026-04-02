e8074ddbf7a76c7a05cab58edc4af3bce1a7fea3
Aman Sarfraz <amansarfraz05@gmail.com>
2026-04-02 22:31:00 +0500

    Time: 3 ms (75.11%) | Memory: 12.4 MB (79.38%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        from collections import Counter
+        
+        count = Counter(s)
+        length = 0
+        odd_found = False
+        
+        for c in count.values():
+            if c % 2 == 0:
+                length += c
+            else:
+                length += c - 1
+                odd_found = True
+        
+        if odd_found:
+            length += 1
+        
+        return length
