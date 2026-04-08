19959f94b876b440bf275a42850a3165f07559ec
Aman Sarfraz <amansarfraz05@gmail.com>
2026-04-08 11:50:24 +0500

    Time: 0 ms (100.00%) | Memory: 12.5 MB (33.70%) - LeetSync

---
+++ 500-keyboard-row/keyboard-row.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def findWords(self, words):
+        row1 = set("qwertyuiop")
+        row2 = set("asdfghjkl")
+        row3 = set("zxcvbnm")
+        
+        res = []
+        
+        for word in words:
+            lower = set(word.lower())
+            if lower <= row1 or lower <= row2 or lower <= row3:
+                res.append(word)
+        
+        return res
