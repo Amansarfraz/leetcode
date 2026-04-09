f51e45580cd3b820f9e0ac23dc019052746931af
Aman Sarfraz <amansarfraz05@gmail.com>
2026-04-09 23:36:45 +0500

    Time: 49 ms (29.32%) | Memory: 12.6 MB (89.85%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def checkInclusion(self, s1, s2):
+        from collections import Counter
+
+        len1, len2 = len(s1), len(s2)
+
+        if len1 > len2:
+            return False
+
+        s1_count = Counter(s1)
+        window_count = Counter(s2[:len1])
+
+        if s1_count == window_count:
+            return True
+
+        for i in range(len1, len2):
+            window_count[s2[i]] += 1
+            window_count[s2[i - len1]] -= 1
+
+            # remove zero-count keys
+            if window_count[s2[i - len1]] == 0:
+                del window_count[s2[i - len1]]
+
+            if window_count == s1_count:
+                return True
+
+        return False
