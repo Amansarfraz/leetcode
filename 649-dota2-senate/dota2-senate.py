2bb68dee1a489ad7064f7abe3d46a5b49455d8a3
Aman Sarfraz <amansarfraz05@gmail.com>
2026-04-13 22:36:47 +0500

    Time: 12 ms (100.00%) | Memory: 12.6 MB (81.64%) - LeetSync

---
+++ 649-dota2-senate/dota2-senate.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+class Solution(object):
+    def predictPartyVictory(self, senate):
+        
+        n = len(senate)
+        radiant = deque()
+        dire = deque()
+
+        # store indices
+        for i, s in enumerate(senate):
+            if s == 'R':
+                radiant.append(i)
+            else:
+                dire.append(i)
+
+        # simulation
+        while radiant and dire:
+            r = radiant.popleft()
+            d = dire.popleft()
+
+            # smaller index wins this round and goes back with updated index
+            if r < d:
+                radiant.append(r + n)
+            else:
+                dire.append(d + n)
+
+        return "Radiant" if radiant else "Dire"
