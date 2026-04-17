bc6ae39fe4d87483b38c06239d4761ab6caecc44
Aman Sarfraz <amansarfraz05@gmail.com>
2026-04-17 22:15:33 +0500

    Time: 88 ms (38.01%) | Memory: 38.1 MB (9.21%) - LeetSync

---
+++ 817-design-hashmap/design-hashmap.py
@@ -0,0 +1,13 @@
+class MyHashMap(object):
+
+    def __init__(self):
+        self.data = [-1] * (10**6 + 1)
+
+    def put(self, key, value):
+        self.data[key] = value
+
+    def get(self, key):
+        return self.data[key]
+
+    def remove(self, key):
+        self.data[key] = -1
