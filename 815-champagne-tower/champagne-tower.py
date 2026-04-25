f8040869466d1ca15b6ff48eb775f52945a28c15
Aman Sarfraz <amansarfraz05@gmail.com>
2026-04-25 21:59:32 +0500

    Time: 64 ms (51.32%) | Memory: 12.4 MB (83.55%) - LeetSync

---
+++ 815-champagne-tower/champagne-tower.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def champagneTower(self, poured, query_row, query_glass):
+        # DP table (rows)
+        dp = [[0.0] * (i + 1) for i in range(query_row + 1)]
+        dp[0][0] = poured
+        
+        for r in range(query_row):
+            for c in range(len(dp[r])):
+                if dp[r][c] > 1:
+                    overflow = (dp[r][c] - 1) / 2.0
+                    dp[r + 1][c] += overflow
+                    dp[r + 1][c + 1] += overflow
+        
+        return min(1, dp[query_row][query_glass])
