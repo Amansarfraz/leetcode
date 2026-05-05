eaa91b1e20040974f5598ea5b65f3f405e9b9228
Aman Sarfraz <amansarfraz05@gmail.com>
2026-05-05 08:35:19 +0500

    Time: 6 ms (51.64%) | Memory: 12.3 MB (77.05%) - LeetSync

---
+++ 919-projection-area-of-3d-shapes/projection-area-of-3d-shapes.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def projectionArea(self, grid):
+        n = len(grid)
+        
+        top = 0
+        front = 0
+        side = 0
+        
+        for i in range(n):
+            row_max = 0
+            col_max = 0
+            
+            for j in range(n):
+                # Top view
+                if grid[i][j] > 0:
+                    top += 1
+                
+                # Row max (front view)
+                row_max = max(row_max, grid[i][j])
+                
+                # Column max (side view)
+                col_max = max(col_max, grid[j][i])
+            
+            front += row_max
+            side += col_max
+        
+        return top + front + side
