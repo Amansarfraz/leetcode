fa06861f5e012d774233956eeeeaab68e4d89b80
Aman Sarfraz <amansarfraz05@gmail.com>
2026-05-05 08:39:24 +0500

    Time: 21 ms (29.17%) | Memory: 14.2 MB (85.42%) - LeetSync

---
+++ 921-spiral-matrix-iii/spiral-matrix-iii.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def spiralMatrixIII(self, rows, cols, rStart, cStart):
+        result = []
+        
+        # directions: right, down, left, up
+        dirs = [(0,1), (1,0), (0,-1), (-1,0)]
+        
+        steps = 1
+        r, c = rStart, cStart
+        
+        result.append([r, c])
+        
+        while len(result) < rows * cols:
+            for i in range(4):
+                dr, dc = dirs[i]
+                
+                # increase step after every 2 directions
+                for _ in range(steps):
+                    r += dr
+                    c += dc
+                    
+                    if 0 <= r < rows and 0 <= c < cols:
+                        result.append([r, c])
+                
+                if i % 2 == 1:
+                    steps += 1
+        
+        return result
