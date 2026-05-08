d1afa307971e27542c57dafc66d9441d40f9bf2f
Aman Sarfraz <amansarfraz05@gmail.com>
2026-05-08 21:01:11 +0500

    Time: 254 ms (70.94%) | Memory: 17.2 MB (6.04%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def totalFruit(self, fruits):
+        left = 0
+        count = {}
+        ans = 0
+        
+        for right in range(len(fruits)):
+            fruit = fruits[right]
+            count[fruit] = count.get(fruit, 0) + 1
+            
+            # More than 2 fruit types
+            while len(count) > 2:
+                left_fruit = fruits[left]
+                count[left_fruit] -= 1
+                
+                if count[left_fruit] == 0:
+                    del count[left_fruit]
+                
+                left += 1
+            
+            ans = max(ans, right - left + 1)
+        
+        return ans
