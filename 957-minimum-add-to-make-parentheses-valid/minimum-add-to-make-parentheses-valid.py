0ac66ceea5f7534c68a57e9cc17437b2e3e83311
Aman Sarfraz <amansarfraz05@gmail.com>
2026-05-10 15:41:15 +0500

    Time: 0 ms (100.00%) | Memory: 12.3 MB (51.66%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def minAddToMakeValid(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        balance = 0
+        additions = 0
+
+        for ch in s:
+            if ch == '(':
+                balance += 1
+            else:
+                if balance > 0:
+                    balance -= 1
+                else:
+                    additions += 1
+
+        return additions + balance
