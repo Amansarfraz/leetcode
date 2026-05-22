d26dd8da0f02c75c744499157d896b5b09a8beb3
Aman Sarfraz <amansarfraz05@gmail.com>
2026-05-22 18:20:54 +0500

    Time: 76 ms (21.21%) | Memory: 16 MB (84.85%) - LeetSync

---
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maxScoreSightseeingPair(self, values):
+        """
+        :type values: List[int]
+        :rtype: int
+        """
+        
+        best = values[0]
+        ans = 0
+        
+        for j in range(1, len(values)):
+            ans = max(ans, best + values[j] - j)
+            best = max(best, values[j] + j)
+        
+        return ans
