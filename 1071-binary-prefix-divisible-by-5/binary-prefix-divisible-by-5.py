8c2bc887a3a15376d036f1233180fb32dac0f163
Aman Sarfraz <amansarfraz05@gmail.com>
2026-05-23 22:42:53 +0500

    Time: 7 ms (52.88%) | Memory: 13.7 MB (33.45%) - LeetSync

---
+++ 1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def prefixesDivBy5(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[bool]
+        """
+        
+        result = []
+        current = 0
+        
+        for bit in nums:
+            
+            # Build binary number incrementally
+            current = (current * 2 + bit) % 5
+            
+            # Check divisibility by 5
+            result.append(current == 0)
+        
+        return result
