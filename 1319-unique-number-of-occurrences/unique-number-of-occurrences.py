b5eada560cd71105ad7225da18480000394f2e83
Aman Sarfraz <amansarfraz05@gmail.com>
2026-06-13 21:52:53 +0500

    Time: 0 ms (100.00%) | Memory: 12.5 MB (52.25%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -0,0 +1,10 @@
+from collections import Counter
+
+class Solution(object):
+    def uniqueOccurrences(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        freq = Counter(arr)
+        return len(freq.values()) == len(set(freq.values()))
