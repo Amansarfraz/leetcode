d171661f6d4eaecf62f92c975a89e2437338d744
Aman Sarfraz <amansarfraz05@gmail.com>
2026-06-14 22:17:45 +0500

    Time: 63 ms (84.00%) | Memory: 12.9 MB (76.00%) - LeetSync

---
+++ 1332-count-vowels-permutation/count-vowels-permutation.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def countVowelPermutation(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        MOD = 10**9 + 7
+
+        a = e = i = o = u = 1
+
+        for _ in range(n - 1):
+            na = (e + i + u) % MOD
+            ne = (a + i) % MOD
+            ni = (e + o) % MOD
+            no = i % MOD
+            nu = (i + o) % MOD
+
+            a, e, i, o, u = na, ne, ni, no, nu
+
+        return (a + e + i + o + u) % MOD
