fffb62a0d644861c7e6ce3f39c81b4f5cb229a00
Aman Sarfraz <amansarfraz05@gmail.com>
2026-06-17 23:16:15 +0500

    Time: 25 ms (74.42%) | Memory: 13.1 MB (72.09%) - LeetSync

---
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.py
@@ -0,0 +1,26 @@
+
+class Solution(object):
+    def countServers(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        m, n = len(grid), len(grid[0])
+
+        rows = [0] * m
+        cols = [0] * n
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]:
+                    rows[i] += 1
+                    cols[j] += 1
+
+        ans = 0
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] and (rows[i] > 1 or cols[j] > 1):
+                    ans += 1
+
+        return ans
