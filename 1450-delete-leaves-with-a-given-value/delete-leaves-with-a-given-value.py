3359b61a4388c8e8c0271ac7caacb413a5f9026f
Aman Sarfraz <amansarfraz05@gmail.com>
2026-06-21 23:06:45 +0500

    Time: 7 ms (25.93%) | Memory: 14.4 MB (86.42%) - LeetSync

---
+++ 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def removeLeafNodes(self, root, target):
+        """
+        :type root: Optional[TreeNode]
+        :type target: int
+        :rtype: Optional[TreeNode]
+        """
+        if not root:
+            return None
+
+        root.left = self.removeLeafNodes(root.left, target)
+        root.right = self.removeLeafNodes(root.right, target)
+
+        if root.val == target and not root.left and not root.right:
+            return None
+
+        return root
