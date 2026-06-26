b3cd474c12edd7a0443e43ab2c0f29b23e535e31
Aman Sarfraz <amansarfraz05@gmail.com>
2026-06-26 21:30:28 +0500

    Time: 115 ms (82.69%) | Memory: 36.3 MB (21.15%) - LeetSync

---
+++ 1488-sort-integers-by-the-power-value/sort-integers-by-the-power-value.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def getKth(self, lo, hi, k):
+        """
+        :type lo: int
+        :type hi: int
+        :type k: int
+        :rtype: int
+        """
+        memo = {1: 0}
+
+        def power(x):
+            if x in memo:
+                return memo[x]
+
+            if x % 2 == 0:
+                memo[x] = 1 + power(x // 2)
+            else:
+                memo[x] = 1 + power(3 * x + 1)
+
+            return memo[x]
+
+        arr = []
+
+        for num in range(lo, hi + 1):
+            arr.append((power(num), num))
+
+        arr.sort()
+
+        return arr[k - 1][1]
