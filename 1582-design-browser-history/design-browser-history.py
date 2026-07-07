d729515cbf8ee9b4477cfb87684b45addf759434
Aman Sarfraz <amansarfraz05@gmail.com>
2026-07-07 22:40:41 +0500

    Time: 38 ms (86.86%) | Memory: 14.9 MB (74.58%) - LeetSync

---
+++ 1582-design-browser-history/design-browser-history.py
@@ -0,0 +1,40 @@
+class BrowserHistory(object):
+
+    def __init__(self, homepage):
+        """
+        :type homepage: str
+        """
+        self.history = [homepage]
+        self.curr = 0
+
+    def visit(self, url):
+        """
+        :type url: str
+        :rtype: None
+        """
+        self.history = self.history[:self.curr + 1]
+        self.history.append(url)
+        self.curr += 1
+
+    def back(self, steps):
+        """
+        :type steps: int
+        :rtype: str
+        """
+        self.curr = max(0, self.curr - steps)
+        return self.history[self.curr]
+
+    def forward(self, steps):
+        """
+        :type steps: int
+        :rtype: str
+        """
+        self.curr = min(len(self.history) - 1, self.curr + steps)
+        return self.history[self.curr]
+
+
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
