c2fd3a272c95648b4eafbded398e26f6826edbfa
Aman Sarfraz <amansarfraz05@gmail.com>
2026-07-13 21:06:05 +0500

    Time: 8 ms (34.48%) | Memory: 12.9 MB (100.00%) - LeetSync

---
+++ 1627-last-moment-before-all-ants-fall-out-of-a-plank/last-moment-before-all-ants-fall-out-of-a-plank.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def getLastMoment(self, n, left, right):
+        """
+        :type n: int
+        :type left: List[int]
+        :type right: List[int]
+        :rtype: int
+        """
+        ans = 0
+
+        for pos in left:
+            ans = max(ans, pos)
+
+        for pos in right:
+            ans = max(ans, n - pos)
+
+        return ans
+        
