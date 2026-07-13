dfff417620e7d171bc0e45ed492c434cbb0b9950
Aman Sarfraz <amansarfraz05@gmail.com>
2026-07-13 21:15:17 +0500

    Time: 134 ms (80.67%) | Memory: 24.9 MB (37.82%) - LeetSync

---
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.py
@@ -0,0 +1,40 @@
+import heapq
+
+class Solution(object):
+    def maxProbability(self, n, edges, succProb, start_node, end_node):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :type succProb: List[float]
+        :type start_node: int
+        :type end_node: int
+        :rtype: float
+        """
+        graph = [[] for _ in range(n)]
+
+        for (u, v), p in zip(edges, succProb):
+            graph[u].append((v, p))
+            graph[v].append((u, p))
+
+        prob = [0.0] * n
+        prob[start_node] = 1.0
+
+        heap = [(-1.0, start_node)]
+
+        while heap:
+            curr_prob, node = heapq.heappop(heap)
+            curr_prob = -curr_prob
+
+            if node == end_node:
+                return curr_prob
+
+            if curr_prob < prob[node]:
+                continue
+
+            for nei, p in graph[node]:
+                new_prob = curr_prob * p
+                if new_prob > prob[nei]:
+                    prob[nei] = new_prob
+                    heapq.heappush(heap, (-new_prob, nei))
+
+        return 0.0
