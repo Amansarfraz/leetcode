d6e9fcc489422d97cfca5579454557c6d48d0a10
Aman Sarfraz <amansarfraz05@gmail.com>
2026-08-09 16:27:34 +0500

    Time: 148 ms (67.71%) | Memory: 13.8 MB (32.29%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.py
+++ 1240-stone-game-ii/stone-game-ii.py
@@ -5,31 +5,35 @@ class Solution(object):
         :rtype: int
         """
         n = len(piles)
-
-        suffix = [0] * (n + 1)
+        
+        # Suffix sums to compute total remaining stones efficiently
+        suffix_sums = [0] * (n + 1)
         for i in range(n - 1, -1, -1):
-            suffix[i] = suffix[i + 1] + piles[i]
-
+            suffix_sums[i] = suffix_sums[i + 1] + piles[i]
+            
         memo = {}
-
-        def dp(i, M):
+        
+        def dfs(i, M):
+            # Base case: no piles left
             if i >= n:
                 return 0
-
-            if 2 * M >= n - i:
-                return suffix[i]
-
+            
+            # Check memoized results
             if (i, M) in memo:
                 return memo[(i, M)]
-
-            best = 0
+            
+            # If current player can take all remaining piles
+            if i + 2 * M >= n:
+                return suffix_sums[i]
+            
+            # Minimize opponent's score to maximize own score
+            max_stones = 0
             for X in range(1, 2 * M + 1):
-                best = max(
-                    best,
-                    suffix[i] - dp(i + X, max(M, X))
-                )
-
-            memo[(i, M)] = best
-            return best
-
-        return dp(0, 1)
+                opponent_score = dfs(i + X, max(M, X))
+                current_score = suffix_sums[i] - opponent_score
+                max_stones = max(max_stones, current_score)
+                
+            memo[(i, M)] = max_stones
+            return max_stones
+            
+        return dfs(0, 1)
