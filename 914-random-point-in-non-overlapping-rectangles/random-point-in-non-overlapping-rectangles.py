a68692fb293962b1968794b0f6d5d9c7464b5470
Aman Sarfraz <amansarfraz05@gmail.com>
2026-08-27 20:19:00 +0500

    Time: 79 ms (65.04%) | Memory: 16.2 MB (17.89%) - LeetSync

--- 914-random-point-in-non-overlapping-rectangles/random-point-in-non-overlapping-rectangles.py
+++ 914-random-point-in-non-overlapping-rectangles/random-point-in-non-overlapping-rectangles.py
@@ -4,28 +4,39 @@ import bisect
 class Solution(object):
 
     def __init__(self, rects):
+        """
+        :type rects: List[List[int]]
+        """
         self.rects = rects
-        self.prefix = []
-        total = 0
+        self.prefix_sums = []
+        total_points = 0
         
-        # 🔥 build prefix sum of areas
         for x1, y1, x2, y2 in rects:
-            area = (x2 - x1 + 1) * (y2 - y1 + 1)
-            total += area
-            self.prefix.append(total)
-        
-        self.total = total
+            # Number of integer points in the current rectangle
+            # Elements are inclusive, so width is (x2 - x1 + 1) and height is (y2 - y1 + 1)
+            points = (x2 - x1 + 1) * (y2 - y1 + 1)
+            total_points += points
+            self.prefix_sums.append(total_points)
 
     def pick(self):
-        # 🔥 pick random point index
-        k = random.randint(1, self.total)
+        """
+        :rtype: List[int]
+        """
+        # Pick a random point index out of the total pool of points
+        target = random.randint(1, self.prefix_sums[-1])
         
-        # find rectangle index
-        idx = bisect.bisect_left(self.prefix, k)
-        x1, y1, x2, y2 = self.rects[idx]
+        # Use binary search to find which rectangle contains this point index
+        rect_idx = bisect.bisect_left(self.prefix_sums, target)
         
-        # pick random point inside rectangle
-        x = random.randint(x1, x2)
-        y = random.randint(y1, y2)
+        # Get the chosen rectangle coordinates
+        x1, y1, x2, y2 = self.rects[rect_idx]
         
-        return [x, y]
+        # Pick a random x and y coordinate inside this specific rectangle
+        random_x = random.randint(x1, x2)
+        random_y = random.randint(y1, y2)
+        
+        return [random_x, random_y]
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(rects)
+# param_1 = obj.pick()
