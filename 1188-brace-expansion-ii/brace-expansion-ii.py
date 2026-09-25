8d801ed6a77301d30d0d60ced643a4fd474a2f2d
Aman Sarfraz <amansarfraz05@gmail.com>
2026-09-25 20:55:21 +0500

    Time: 44 ms (6.67%) | Memory: 12.9 MB (0.00%) - LeetSync

--- 1188-brace-expansion-ii/brace-expansion-ii.py
+++ 1188-brace-expansion-ii/brace-expansion-ii.py
@@ -1,42 +1,19 @@
 class Solution(object):
     def braceExpansionII(self, expression):
-        def product(a, b):
-            return {x + y for x in a for y in b}
+        """:type expression: str
 
-        stack = []
-        curr = {""}
-        union = set()
+        :rtype: List[str]
+        """
+        def dfs(exp):
+            j = exp.find('}')
+            if j == -1:
+                s.add(exp)
+                return
+            i = exp.rfind('{', 0, j - 1)
+            a, c = exp[:i], exp[j + 1:]
+            for b in exp[i + 1:j].split(','):
+                dfs(a + b + c)
 
-        i = 0
-        while i < len(expression):
-            ch = expression[i]
-
-            if ch.isalpha():
-                j = i
-                while j < len(expression) and expression[j].isalpha():
-                    j += 1
-
-                word = expression[i:j]
-                curr = product(curr, {word})
-                i = j - 1
-
-            elif ch == '{':
-                stack.append((union, curr))
-                union = set()
-                curr = {""}
-
-            elif ch == ',':
-                union |= curr
-                curr = {""}
-
-            elif ch == '}':
-                union |= curr
-                prev_union, prev_curr = stack.pop()
-
-                curr = product(prev_curr, union)
-                union = prev_union
-
-            i += 1
-
-        result = sorted(list(union | curr))
-        return result
+        s = set()
+        dfs(expression)
+        return sorted(s)
